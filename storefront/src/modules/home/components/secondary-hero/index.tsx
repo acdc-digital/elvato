b16bce49dcdf4dc8d081ca72da6658719e2bb115
Matthew Simon <msimon@acdc.digital>
2026-03-21 23:35:32 -0300

    Remove secondary hero top border; smooth scroll with offset for anchor nav

--- storefront/src/modules/home/components/secondary-hero/index.tsx
+++ storefront/src/modules/home/components/secondary-hero/index.tsx
@@ -3,7 +3,7 @@ import LocalizedClientLink from "@modules/common/components/localized-client-lin
 
 const SecondaryHero = () => {
   return (
-    <div className="w-full border-t border-black overflow-hidden min-h-[50vh] lg:h-[50vh]">
+    <div className="w-full overflow-hidden min-h-[50vh] lg:h-[50vh]">
       <div className="grid grid-cols-1 lg:grid-cols-2 relative h-full">
         
         {/* Left Panel - Image */}
